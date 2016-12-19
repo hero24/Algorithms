542be5fe0fba1e580bd691e0fa9145f8a939882c
hero24 <hero24@interia.pl>
2016-12-19 14:37:51 +0000

    Update bubble_sort.h

--- c/sorting/bubble_sort.h
+++ c/sorting/bubble_sort.h
@@ -1,3 +1,19 @@
+/*
+ *  It's important to enjoy life while you still can!
+ *  ~ Victoria @ RED
+ */
+
+/* bubble sort for sorting integer arrays*/
 void bubble_sort(int arr[],int length)
 {
+  int i,j;
+  for (i = length-1; i > 0;i--){
+    for(j = 0; j < i;j++){
+      if (arr[j] > arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
 }
