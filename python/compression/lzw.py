9a0aca974984363033a83b0d0e590040a8fb3391
hero24 <hero24@interia.pl>
2018-04-27 18:49:13 +0100

    removal of dict_size variable

--- python/compression/lzw.py
+++ python/compression/lzw.py
@@ -7,8 +7,7 @@ class BadCompressionError(Exception):
 class LZW:
     @staticmethod
     def compress(uncompressed):
-        dict_size = 256
-        hashmap = {chr(i): i for i in range(dict_size)}
+        hashmap = {chr(i): i for i in range(256)}
         w = ""
         result = []
         for c in uncompressed:
@@ -17,27 +16,25 @@ class LZW:
                 w = wc
             else:
                 result.append(hashmap[w])
-                hashmap[wc] = dict_size
-                dict_size += 1
+                hashmap[wc] = len(hashmap) 
                 w = c
         if w:
             result.append(hashmap[w])
         return result
 
     def decompress(compressed):
-        dict_size = 256
-        hashmap = {i:chr(i) for i in range(dict_size+1)}
+        hashmap = {i:chr(i) for i in range(256)}
         w = chr(compressed[0])
         result = w
         for i in compressed[1:]:
             if i in hashmap:
                 entry = hashmap[i]
-            elif i == dict_size:
+            elif i == len(hashmap):
                entry = w + w[0]
             else:
                 raise BadCompressionError("Corrupted data")
             result += entry
-            hashmap[dict_size] = w + entry[0]
-            dict_size += 1
+            hashmap[len(hashmap)] = w + entry[0]
             w = entry
         return result
+
