059fee4f06dab8d2e2a64be25225a8911d9b5c44
hero24 <hero24@interia.pl>
2021-05-21 19:26:36 +0100

    Add shift - based implementation

--- c/searching/rk.h
+++ c/searching/rk.h
@@ -31,12 +31,32 @@ int rk(char sample[], char test[]){
                 bM_1 = (B_N * bM_1) % PRIME_N;
         }
         for(i = 0; Hs != Ht; i++){
-                // All the prime number multupilications could be optimized away using shifts
-                // and as we know shifts are much faster operations than multiplications.
-                //Ht = (Ht + (PRIME_N << B_SH) - index_s(test[i]) * bM_1) % PRIME_N; ...
                 Ht = (Ht + B_N * PRIME_N - index_s(test[i]) * bM_1) % PRIME_N;
-                //Ht = ((Ht << B_SH) + index_s(test[i+M])) % PRIME_N;
                 Ht = (Ht * B_N + index_s(test[i+M])) % PRIME_N;
+                if (i > N-M)
+                        return -1;
+        }
+        return i;
+}
+
+/*
+* Rabin-Karp algortihm optimized to use shifts instead of 
+* multiplication, as we know shifts are much faster than
+* multiplications
+*/
+int rk_shift(char sample[], char test[]){
+        int i, M = strlen(sample), N = strlen(test);
+        unsigned long bM_1 = 1, Hs = 0, Ht = 0;
+        for(i = 0; i < M; i++){
+                Hs = (Hs * B_N + index_s(sample[i])) % PRIME_N;
+                Ht = (Ht * B_N + index_s(test[i])) % PRIME_N;
+        }
+        for(i = 1; i < M; i++){
+                bM_1 = (B_N * bM_1) % PRIME_N;
+        }
+        for(i = 0; Hs != Ht; i++){
+                Ht = (Ht + (PRIME_N << B_SH) - index_s(test[i]) * bM_1) % PRIME_N;
+                Ht = ((Ht << B_SH) + index_s(test[i+M])) % PRIME_N;
                 if (i > N-M)
                         return -1;
         }
