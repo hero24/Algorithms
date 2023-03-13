28052e7acc88a2cbe26db15b5788edb493ed4045
hero24 <hero24@interia.pl>
2023-03-13 19:28:47 +0000

    python/capi: add option for compiling with c modules

--- python/__init__.py
+++ python/__init__.py
@@ -4,14 +4,15 @@ how close they were to success when they gave up.
 -Thomas A. Edison
 """
 
-def _get_dirs():
+exclude = ["dist",
+           "__pycache__",
+           "build",
+           "lib",
+           "algorithms"
+]
+
+def _get_dirs(exclude):
     from os import walk
-    exclude = ["dist",
-               "__pycache__",
-               "build",
-               "lib",
-               "algorithms"
-    ]
     dirs = {"hero24/algorithms" : "."}
     for _, d1, _ in walk("."):
         for d in d1:
@@ -24,19 +25,25 @@ def _get_dirs():
 if __name__ == "__main__":
     from distutils.core import setup, Extension
     from sys import argv
-
+    ext_modules = []
+    args = {
+        "name":"algorithms",
+        "version":"0.2",
+        "description":"Python implementations of various algorithms",
+        "author":"hero24"
+    }
+    if "capi" in argv:
+        args["ext_modules"] = [
+            Extension("hero24/algorithms/capi/rabin_karp", ["capi/rk.c"]),
+            Extension("hero24/algorithms/capi/boyer_moore", ["capi/bm.c"]),
+            Extension("hero24/algorithms/capi/knuth_morris_pratt", ["capi/kmp.c"]),
+            Extension("hero24/algorithms/capi/hashing", ["capi/hashing.c"])
+        ]
+        argv.remove("capi")
+    else:
+        exclude += ["capi"]
     argv += ['install']
-    dirs = _get_dirs()
-    packs = [k for k in dirs]
+    args["package_dir"] = _get_dirs(exclude)
+    args["packages"] = [k for k in args["package_dir"]]
     
-    setup(name="algorithms",
-      version="0.1",
-      description="Python implementations of various algorithms",
-      author="hero24",
-      package_dir=dirs,
-      packages=packs,
-            ext_modules=[Extension("hero24/algorithms/capi/rabin_karp", ["capi/rk.c"]),
-              Extension("hero24/algorithms/capi/boyer_moore", ["capi/bm.c"]),
-              Extension("hero24/algorithms/capi/knuth_morris_pratt", ["capi/kmp.c"]),
-              Extension("hero24/algorithms/capi/hashing", ["capi/hashing.c"])]
-      )
+    setup(**args)
