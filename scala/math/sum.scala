ba8bb5ee46a1373a76396c0e65c6de5a13c53d38
hero24 <hero24@interia.pl>
2023-03-27 17:53:13 +0100

    scala: fix compilation issues
    fix compilation issues caused by functions not being wrapped into objects

--- scala/math/sum.scala
+++ scala/math/sum.scala
@@ -1,9 +1,12 @@
+package hero24.algorithms.math
 /*
   "Corpus delicti" ~ Julian Tuwim
 */
 
-// Gausian sum for first n conescutive numbers
-def gaussian_sum(n:Int) = (n/2) * (n + 1)
+object GaussianSum {
+  // Gausian sum for first n conescutive numbers
+  def gaussian_sum(n:Int) = (n/2) * (n + 1)
 
-// Gausian sum for range of numbers from s to n
-def gaussian_sum(s:Int,n:Int) = (n/2 + s/2) * (n - s + 1)
+  // Gausian sum for range of numbers from s to n
+  def gaussian_sum(s:Int,n:Int) = (n/2 + s/2) * (n - s + 1)
+}
