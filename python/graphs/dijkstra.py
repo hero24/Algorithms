e966f7a00fe04311d434190942623873e01c4c99
Herakliusz Lipiec <hero24@interia.pl>
2024-07-10 12:17:45 +0100

    python/graphs: add dijkstra shortest path
    Signed-off-by: Herakliusz Lipiec <hero24@interia.pl>

--- python/graphs/dijkstra.py
+++ python/graphs/dijkstra.py
@@ -2,6 +2,8 @@
 # experience treacherous, judgment difficult."
 # ~ Hippocrates
 
+NEG_CYCLE = True
+
 def dikstra(graph, statring_point):
     """
     Dijkstra shortest path algorithm
@@ -25,3 +27,27 @@ def dikstra(graph, statring_point):
                 previous[neighbor] = current
         unvisited.remove(current)
     return previous, shortest
+
+
+def bellman_ford(graph, starting_point):
+    """
+    Shortest path algorithm that accounts for
+    negative cycles. Returns True if graph contains
+    negative.
+    """
+    inf = float("inf")
+    unvisited = [node for node in graph.yield_nodes()]
+    shortest = {node:inf for node in unvisited}
+    shortest[starting_point] = 0
+    for node in graph.yield_nodes():
+        for neighbour in graph.neighbours(node):
+            weight = graph.get_weight(node, neighbour)
+            n_weight = shortest[node] + weight
+            if shortest[node] != inf and n_weight < shortest[neighbour]:
+                shortest[neighbour] = shortest[node] + weight
+    for node in graph.yield_nodes():
+        for neighbour in graph.neighbours(node):
+            weight = shortest[node] + graph.get_weight(node, neighbour)
+            if shortest[node] != inf and weight < shortest[node]:
+                return NEG_CYCLE
+    return shortest
